bcbc03f4374cf5a416f4ee6fa06cda13126c76b4
glupo <philip.kurlovich@gmail.com>
2015-07-15 16:39:52 +0300

    Update account.rb

--- lib/traktr/account.rb
+++ lib/traktr/account.rb
@@ -1,12 +1,12 @@
 module Traktr
   class Account < Endpoint
     def settings
-      response = self.class.post("/" + File.join("settings", @client.api_key), body: @auth.to_json, headers: { 'Content-Type' => 'application/json'})
+      response = self.class.post("/" + File.join("settings", @client.api_key), body: @auth.to_json, headers: { 'Content-Type' => 'application/json', 'trakt-api-key' => File.join("settings", @client.api_key), 'trakt-api-version' => 2})
       parse_response(response)
     end
 
     def test
-      response = self.class.post("/" + File.join("test", @client.api_key), body: @auth.to_json, headers: { 'Content-Type' => 'application/json'})
+      response = self.class.post("/" + File.join("settings", @client.api_key), body: @auth.to_json, headers: { 'Content-Type' => 'application/json', 'trakt-api-key' => File.join("settings", @client.api_key), 'trakt-api-version' => 2})
       parse_response(response)
     end
   end
